a534adedb6c7031bbbb08668e16aae8421402213
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 16:37:57 +0530

    Update ass5.py

--- python/patterns1/assignments/ass5.py
+++ python/patterns1/assignments/ass5.py
@@ -1,1 +1,24 @@
+"""
+ A
+ BB
+ CCC
+"""
+from os import *
+from sys import *
+from collections import *
+from math import *
 
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+
+
+N = int(input())
+row = 1
+while row <= N:
+    col = 1
+    while col <= row:
+        print(chr(64+row), end="")
+        col = col+1
+    row = row+1
+    print()
