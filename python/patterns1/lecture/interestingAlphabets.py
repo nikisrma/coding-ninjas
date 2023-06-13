0ac8d19e3f8098de77aad327b8f1824f2550e578
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 16:16:00 +0530

    Update interestingAlphabets.py

--- python/patterns1/lecture/interestingAlphabets.py
+++ python/patterns1/lecture/interestingAlphabets.py
@@ -1,11 +1,23 @@
-## Read input as specified in the question
-## Print the required output in given format
+"""
+E
+DE
+CDE
+BCDE
+ABCDE
+"""
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
 n = int(input())
 i = 1
-while(i<=n):
-    j=1
-    while(j<=i):
-        print(i,end="")
+while i<=n:
+    j = 1
+    while j<=i:
+        print(chr(64+n+j-i),end="")
         j = j+1
     print()
     i = i+1
