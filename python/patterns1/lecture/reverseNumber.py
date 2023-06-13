25665ae8081fe38ef49d1e7db9e411a3ececf588
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 16:07:50 +0530

    Update reverseNumber.py

--- python/patterns1/lecture/reverseNumber.py
+++ python/patterns1/lecture/reverseNumber.py
@@ -1,1 +1,18 @@
+"""
+1
+21
+321
+4321
 
+"""
+## Read input as specified in the question
+## Print the required output in given format
+n = int(input())
+i = 1
+while(i<=n):
+    j=i
+    while(j>=1):
+        print(j,end="")
+        j = j-1
+    print()
+    i = i+1
