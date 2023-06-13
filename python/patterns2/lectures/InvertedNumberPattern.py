1eb77292144c78d02a855d930cfad1e384822110
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 17:45:38 +0530

    Update InvertedNumberPattern.py

--- python/patterns2/lectures/InvertedNumberPattern.py
+++ python/patterns2/lectures/InvertedNumberPattern.py
@@ -1,1 +1,18 @@
+"""
+4444
+333
+22
+1
+"""
+## Read input as specified in the question
+## Print the required output in given format
+n = int(input())
+i=1
+while i<=n:
+    j = 1
+    while j<=n-i+1:
+        print(n-i+1,end='')
+        j = j+1
+    print()
+    i = i+1
 
