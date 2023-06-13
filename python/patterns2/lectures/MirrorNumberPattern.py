8632def32aff341b55a2cf80ec7439de9758ee48
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 17:33:18 +0530

    Update MirrorNumberPattern.py

--- python/patterns2/lectures/MirrorNumberPattern.py
+++ python/patterns2/lectures/MirrorNumberPattern.py
@@ -1,1 +1,23 @@
+"""
+   1
+  12
+ 123
+1234
 
+"""
+## Read input as specified in the question
+## Print the required output in given format
+
+n = int(input())
+i = 1
+while i<=n:
+    j=1
+    spaces=1
+    while spaces<n-i+1:
+        print(' ',end='')
+        spaces = spaces +1
+    while j<=i:
+        print(j,end="")
+        j= j+1
+    print()
+    i=i+1
