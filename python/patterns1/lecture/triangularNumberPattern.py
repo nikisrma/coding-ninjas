f5c144090ae7b943c3d7faa2941261a170fb9def
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 16:03:49 +0530

    Update triangularNumberPattern.py

--- python/patterns1/lecture/triangularNumberPattern.py
+++ python/patterns1/lecture/triangularNumberPattern.py
@@ -1,1 +1,18 @@
+"""
+1
+22
+333
+4444
 
+"""
+## Read input as specified in the question
+## Print the required output in given format
+n = int(input())
+i = 1
+while(i<=n):
+    j=1
+    while(j<=i):
+        print(i,end="")
+        j = j+1
+    print()
+    i = i+1
