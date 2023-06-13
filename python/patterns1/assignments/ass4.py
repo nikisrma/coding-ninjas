adad607ef19af7bf73a58311df5ad898817d5a06
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 16:34:17 +0530

    Update ass4.py

--- python/patterns1/assignments/ass4.py
+++ python/patterns1/assignments/ass4.py
@@ -1,1 +1,22 @@
+"""
+1234
+123
+12
+1
+"""
+from math import *
+from collections import *
+from sys import *
+from os import *
 
+## Read input as specified in the question.
+## Print output as specified in the question.
+n = int(input())
+i = 1
+while i<=n:
+    j = 1
+    while j<=n-i+1:
+        print(j,end='')
+        j = j+1
+    print()
+    i=i+1
