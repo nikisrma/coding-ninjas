406dc4f012c0c10c14dbc258acecd8199219f287
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-13 16:20:21 +0530

    Update ass1.py

--- python/patterns1/assignments/ass1.py
+++ python/patterns1/assignments/ass1.py
@@ -1,1 +1,23 @@
+"""
+1
+11
+111
+1111
+"""
 
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+n = int(input())
+i = 1
+while i<=n:
+    j= 1
+    while j<=i:
+        print(1,end='')
+        j = j+1
+    print()
+    i = i+1
