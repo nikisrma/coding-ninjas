d671f0f19292ecb40de7e27c08b61053f7c37b5c
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-14 18:13:51 +0530

    Create rotateArray.py
    Rotate Array

---
+++ python/searching & sorting/rotateArray.py
@@ -0,0 +1,40 @@
+from sys import stdin
+
+
+def rotate(arr, n, d):
+    #Your code goes here
+    temp = arr[d:]
+    for i in range(d):
+        temp.append(arr[i])
+    printList(temp, n)
+
+
+
+
+#Taking Input Using Fats I/O
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+    if n == 0:
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+#to print the array/list 
+def printList(arr, n) : 
+    for i in range(n) :
+        print(arr[i], end = " ")
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    d = int(stdin.readline().rstrip())
+    rotate(arr, n, d)
+    
+    t -= 1
