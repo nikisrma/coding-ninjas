5667cf4c32618c174c651de6cddbf1345083c3fe
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-14 16:07:11 +0530

    binary search

---
+++ python/searching & sorting/binarySearch1.py
@@ -0,0 +1,43 @@
+
+from sys import stdin
+
+
+def binarySearch(arr, n, x):
+    start = 0
+    end = len(arr)-1
+    while start <= end:
+        mid = (start+end)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
+    #Your code goes here
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().strip())
+    
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+
+#main
+arr, n = takeInput()
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    
+    x = int(input().strip())    
+    print(binarySearch(arr, n, x))
+
+    t -= 1
