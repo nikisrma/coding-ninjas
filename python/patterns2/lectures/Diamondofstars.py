729b39f14a6ea3b51707b5bbe71b3f04bd05283a
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-14 09:48:21 +0530

    Update Diamondofstars.py

--- python/patterns2/lectures/Diamondofstars.py
+++ python/patterns2/lectures/Diamondofstars.py
@@ -1,1 +1,31 @@
+"""
+  *
+ ***
+*****
+ ***
+  *
+"""
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+n = int(input())
+m = n//2 + 1
+for i in range(1, m+1, 1):
+    for sp in range(m-i):
+        print(' ', end='')
+
+    for j in range(2*i - 1):
+        print('*', end='')
+
+    print()
+
+for i in range(1, m, 1):
+    for sp in range(i):
+        print(' ', end='')
+
+    for j in range(1, n-2*i + 1, 1):
+        print('*', end='')
+    print()
 
