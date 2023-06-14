ec68482653bcd33e6caed49c3c68754c895def65
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-14 14:47:04 +0530

    Array Sum1

---
+++ python/arrays & lists/arraySum1.py
@@ -0,0 +1,12 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+n = int(input())
+li =[int(x) for x in input().split()]
+print(sum(li))
