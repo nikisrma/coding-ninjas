feb849697c87ed0c73391cd01abf16ef2760edc8
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-14 10:12:05 +0530

    Update ass3.py

--- python/patterns2/assignments/ass3.py
+++ python/patterns2/assignments/ass3.py
@@ -1,1 +1,31 @@
+"""
+     1
+    212
+   32123
+  4321234
+ 543212345
+"""
+from os import *
+from sys import *
+from collections import *
+from math import *
 
+
+num=int(input())
+i=1
+while num>=i:
+    spaces=1
+    while spaces<=(num-i):
+        print(" ",end="")
+        spaces=spaces+1
+    k=i
+    while k>=1:
+        print(k,end="")
+        k=k-1
+    j=2
+    while j<=i:
+        print(j,end="")
+        j=j+1
+
+    print()
+    i += 1
