afb0874376532a432b9821541e5ad2cdfd58e23a
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-14 09:55:12 +0530

    Update ass1.py

--- python/patterns2/assignments/ass1.py
+++ python/patterns2/assignments/ass1.py
@@ -1,1 +1,24 @@
-
+"""
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+"""
+n = int(input())
+i = 1
+while i<=n:
+    j=1
+    while j<=i:
+        print(j,end='')
+        j= j+1
+    spaces = 1
+    while spaces<=2*(n-i):
+        print(' ',end='')
+        spaces += 1
+    k=i
+    while k>=1:
+        print(k,end='')
+        k -= 1
+    print()
+    i = i+1
