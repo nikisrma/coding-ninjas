5a64e407f7d735e6a590f812b251480507017b82
Nikita sharma <55843413+nikisrma@users.noreply.github.com>
2023-06-14 14:55:18 +0530

    Find Unique

---
+++ python/arrays & lists/FindUnique.py
@@ -0,0 +1,33 @@
+import sys
+
+def findUnique(arr, n) :
+    #Your code goes here
+    for el in arr:
+        if arr.count(el)==1:
+            return el
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(sys.stdin.readline().rstrip())
+
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, sys.stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(sys.stdin.readline().rstrip())
+
+while t > 0 :
+
+    arr, n = takeInput()
+    print(findUnique(arr, n))
+
+    t -= 1
